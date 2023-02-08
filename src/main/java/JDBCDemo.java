2c5140a379c2404ee7f5faf093a10e62ce5309e1
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2023-02-07 21:51:15 -0600

    jdbc

--- src/main/java/JDBCDemo.java
+++ src/main/java/JDBCDemo.java
@@ -1,23 +1,19 @@
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.sql.DriverManager;
-//@WebServlet(name= "jdbc", urlPatterns = "jdbc")
-//public class JDBCDemo extends HttpServlet {
-//    public static void main(String[] args) throws Exception {
-//    Class.forName("com.mysql.jdbc.Driver");
-//
-//    String url = "";
-//    String username = "";
-//    String password = "";
-//    DriverManager.getConnection(url,username, password);
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//    }
-//}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCDemo {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/student";
+        String username = "root";
+        String password = "codeup";
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        String sql = "INSERT INTO student (id, name, gender,birthday,score,email,tel,status) VALUES ('1','aaa','M',1999-12-12,8.8,'asd@qq.com',12345678,1);";
+
+        Statement stmt = conn.createStatement();
+
+        stmt.execute(sql);
+    }
+}
